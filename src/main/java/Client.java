2a59b020f1d8dfe27ba1f240bc52d9c011839f47
Faisal <faisal.hassan@tracxn.com>
2024-01-13 13:07:05 +0530

    echo command added

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client implements Runnable {
 
@@ -19,12 +21,17 @@ public class Client implements Runnable {
                 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         ) {
             String inputLine;
+            List<String> commandList = new ArrayList<>();
             while ((inputLine = in.readLine()) != null) {
-                System.out.println("Client Command : " + inputLine);
-                if (inputLine.equals("ping")) {
-                    out.print("+PONG\r\n");
-                    out.flush();
-                }
+                commandList.add(inputLine);
+            }
+            if (commandList.get(commandList.size() - 1).equalsIgnoreCase("ping")) {
+                out.println("+PONG");
+                out.flush();
+            } else if (commandList.get(commandList.size() - 3).equalsIgnoreCase("echo")) {
+                int len = Integer.parseInt(commandList.get(commandList.size() - 2).substring(1));
+                out.println("$" + len + commandList.get(commandList.size() - 1));
+                out.flush();
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
